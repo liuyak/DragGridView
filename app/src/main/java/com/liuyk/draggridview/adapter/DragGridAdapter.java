f6af8e8c2357b91aff032353579820f0f79a1f81
liuyakui <yakui1004>
2020-09-07 10:52:12 +0800

    扩展listview拖拽

--- app/src/main/java/com/liuyk/draggridview/adapter/DragGridAdapter.java
+++ app/src/main/java/com/liuyk/draggridview/adapter/DragGridAdapter.java
@@ -13,6 +13,7 @@ import com.liuyk.draggridview.model.Channel;
 /**
  * 拖拽GridView适配器
  * <p>
+ *
  * @author liuyk
  */
 public class DragGridAdapter extends AppBaseAdapter<Channel> {
@@ -24,10 +25,12 @@ public class DragGridAdapter extends AppBaseAdapter<Channel> {
     @Override
     protected void bindViewHolder(int position, ViewHolder viewHolder, View convertView, ViewGroup parent) {
         if (position == 0) {
-            ((DragViewHolder) viewHolder).channelName.setTextColor(Color.WHITE);
+            ((DragViewHolder) viewHolder).channelName.setTextColor(Color.parseColor("#ffffff"));
             ((DragViewHolder) viewHolder).channelName.setBackgroundResource(R.drawable.subscription_immutable_item_background);
+        } else {
+            ((DragViewHolder) viewHolder).channelName.setBackgroundColor(Color.parseColor("#ffffff"));
+            ((DragViewHolder) viewHolder).channelName.setText(getItem(position).getChannelName());
         }
-        ((DragViewHolder) viewHolder).channelName.setText(getItem(position).getChannelName());
     }
 
     @Override
@@ -35,12 +38,12 @@ public class DragGridAdapter extends AppBaseAdapter<Channel> {
         return new DragViewHolder(inflater.inflate(R.layout.drag_channel_grid_view, parent, false));
     }
 
-    private final class DragViewHolder extends ViewHolder {
+    static final class DragViewHolder extends ViewHolder {
         private TextView channelName;
 
         public DragViewHolder(View convertView) {
             super(convertView);
-            channelName = (TextView) convertView.findViewById(R.id.channel_name);
+            channelName = convertView.findViewById(R.id.channel_name);
         }
     }
 
